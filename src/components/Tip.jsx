3a0f348ee30795f2d624ccd6c077c48d7e29bed9
Alexandre LE CORRE <a.elreco@gmail.com>
2022-12-30 00:01:23 +0100

    Add open state on i

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -4,13 +4,20 @@ import { Popover } from '@headlessui/react'
 export default function Tip({ image }) {
   return (
     <Popover className="absolute inset-y-0 right-0 flex items-center pr-2">
-      <Popover.Button className="relative -ml-px inline-flex items-center rounded-full shadow-sm bg-indigo-500 p-1 text-sm font-medium text-gray-700 hover:bg-indigo-300 focus:border-0 focus:outline-none focus:ring-0">
-        <InformationCircleIcon className="h-5 w-5 text-white" aria-hidden="true" />
-      </Popover.Button>
+      {({ open }) => (
+        <>
+          <Popover.Button as="div" className="cursor-pointer relative">
+            <InformationCircleIcon
+              className={`relative z-50 h-5 w-5 ${open ? 'text-gray-800' : 'text-indigo-500'}`}
+              aria-hidden="true"
+            />
+          </Popover.Button>
 
-      <Popover.Panel className="absolute border rounded shadow bg-white p-4 w-[100px] ml-10 mt-3 left-0 top-0 z-10">
-        <img src={`/${image}.svg`} alt="" />
-      </Popover.Panel>
+          <Popover.Panel className="absolute overflow-hidden border rounded shadow bg-white p-4 w-[100px] ml-10 mt-3 left-0 top-0 z-10">
+            <img src={`/${image}.svg`} alt="" />
+          </Popover.Panel>
+        </>
+      )}
     </Popover>
   )
 }
